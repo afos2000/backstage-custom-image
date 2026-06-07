7db7d505648f8d1da27bf32556ad30c931474e36
afos2000 <afortegast@gmail.com>
2026-06-07 21:32:39 +0200

    Add authPlugin feature to createApp

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,3 +1,8 @@
 import { createApp } from '@backstage/frontend-defaults';
+import authPlugin from '@backstage/plugin-auth';
 
-export default createApp();
+export default createApp({
+  features: [
+    authPlugin,
+  ],
+});
